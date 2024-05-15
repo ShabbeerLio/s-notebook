2eaed8557745be3421b6023daf2457bb455c7cd5
ShabbeerLio <108278955+ShabbeerLio@users.noreply.github.com>
2024-05-15 12:07:49 +0530

    Update Login.js

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -7,7 +7,7 @@ const Login = (props) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:8000/api/auth/login", {
+        const response = await fetch(`${host}/api/auth/login`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
