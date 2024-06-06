6b55b43a3fd91e8cf8cd9f35786067f49c3712b6
Shabbeer <shabbeer@Shabbeers-MacBook-Air.local>
2024-06-06 11:32:41 +0530

    members updated

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,25 +1,106 @@
-import React  from 'react'
+import React from "react";
+import m1 from "../Components/Assets/Shabbeer.png";
+import m2 from "../Components/Assets/harsh.png";
+import m3 from "../Components/Assets/ankit.png";
 
 const About = () => {
-
   return (
-    <div className='container' >
+    <div className="container">
       <h2>About</h2>
-      <p>In an era where information overload is the norm, the need for efficient note-taking solutions has never been more pressing. “S-NoteBook”-This project seeks to address this need by crafting a website that serves as a beacon of productivity and organization in the vast expanse of the internet.</p>
-      <p>At its core, this project is driven by a commitment to simplicity, usability, and innovation. The note-taking website we envision will not only streamline the process of capturing and organizing information but will also empower users to unleash their creativity, enhance their productivity, and foster collaboration.</p>
-      <p>But our ambition extends beyond mere functionality. We aspire to create a digital platform that resonates with users on a deeper level, providing not just a tool, but an experience. Through thoughtful design, intuitive features, and seamless integration, our website will serve as a trusted companion in the pursuit of knowledge and achievement.</p>
-      <p>Furthermore, we recognize the importance of accessibility and inclusivity in today's digital landscape. Our note-taking website will be designed with a user-centric approach, ensuring that individuals of all abilities can benefit from its features and functionalities.</p>
+      <p>
+        In an era where information overload is the norm, the need for efficient
+        note-taking solutions has never been more pressing. “S-NoteBook”-This
+        project seeks to address this need by crafting a website that serves as
+        a beacon of productivity and organization in the vast expanse of the
+        internet.
+      </p>
+      <p>
+        At its core, this project is driven by a commitment to simplicity,
+        usability, and innovation. The note-taking website we envision will not
+        only streamline the process of capturing and organizing information but
+        will also empower users to unleash their creativity, enhance their
+        productivity, and foster collaboration.
+      </p>
+      <p>
+        But our ambition extends beyond mere functionality. We aspire to create
+        a digital platform that resonates with users on a deeper level,
+        providing not just a tool, but an experience. Through thoughtful design,
+        intuitive features, and seamless integration, our website will serve as
+        a trusted companion in the pursuit of knowledge and achievement.
+      </p>
+      <p>
+        Furthermore, we recognize the importance of accessibility and
+        inclusivity in today's digital landscape. Our note-taking website will
+        be designed with a user-centric approach, ensuring that individuals of
+        all abilities can benefit from its features and functionalities.
+      </p>
       <h3>OBJECTIVES AND SCOPES:</h3>
-      <p>User-Friendly Interface: Design an intuitive and visually appealing user interface that allows users to easily create, edit, and organize their notes.</p>
-      <p>Efficient Note-Taking Tools: Implement a variety of note-taking tools such as text formatting options, multimedia integration, and annotation features to cater to diverse user needs and preferences.</p>
-      <p>Organization and Search Capabilities: Develop robust organization features including categorization, tagging, and search functionalities to enable users to quickly locate and retrieve their notes.</p>
-      <p>Cross-Platform Compatibility: Ensure compatibility across various devices and platforms, including desktop computers, tablets, and smartphones, to provide users with seamless access to their notes anytime, anywhere.</p>
-      <p>Collaboration Features: Incorporate collaborative features such as real-time editing, sharing, and commenting to facilitate teamwork and knowledge sharing among users.</p>
-      <p>Security and Privacy Measures: Implement stringent security protocols and privacy measures to safeguard users' sensitive information and ensure data integrity.</p>
+      <p>
+        User-Friendly Interface: Design an intuitive and visually appealing user
+        interface that allows users to easily create, edit, and organize their
+        notes.
+      </p>
+      <p>
+        Efficient Note-Taking Tools: Implement a variety of note-taking tools
+        such as text formatting options, multimedia integration, and annotation
+        features to cater to diverse user needs and preferences.
+      </p>
+      <p>
+        Organization and Search Capabilities: Develop robust organization
+        features including categorization, tagging, and search functionalities
+        to enable users to quickly locate and retrieve their notes.
+      </p>
+      <p>
+        Cross-Platform Compatibility: Ensure compatibility across various
+        devices and platforms, including desktop computers, tablets, and
+        smartphones, to provide users with seamless access to their notes
+        anytime, anywhere.
+      </p>
+      <p>
+        Collaboration Features: Incorporate collaborative features such as
+        real-time editing, sharing, and commenting to facilitate teamwork and
+        knowledge sharing among users.
+      </p>
+      <p>
+        Security and Privacy Measures: Implement stringent security protocols
+        and privacy measures to safeguard users' sensitive information and
+        ensure data integrity.
+      </p>
       <h3>PURPOSE</h3>
-      <p>A web note-taking project aims to revolutionize the way users capture, organize, and utilize information by providing a digital platform that is efficient, accessible, and user-friendly. Its purpose is to streamline the note-taking process, enhance productivity, foster collaboration, and ensure the security and privacy of users' data. Through features such as organization, customization, and collaboration tools, the project empowers users to manage their tasks, projects, and ideas effectively, ultimately facilitating success in both personal and professional endeavors</p>
+      <p>
+        A web note-taking project aims to revolutionize the way users capture,
+        organize, and utilize information by providing a digital platform that
+        is efficient, accessible, and user-friendly. Its purpose is to
+        streamline the note-taking process, enhance productivity, foster
+        collaboration, and ensure the security and privacy of users' data.
+        Through features such as organization, customization, and collaboration
+        tools, the project empowers users to manage their tasks, projects, and
+        ideas effectively, ultimately facilitating success in both personal and
+        professional endeavors
+      </p>
+      <h2>Members</h2>
+      <div className="container about-members d-flex justify-content-between">
+        <div className="members-card">
+          <div className="card-imag">
+            <img src={m1} alt="" />
+          </div>
+          <h5>Md Shabbeer</h5>
+        </div>
+        <div className="members-card">
+          <div className="card-imag">
+            <img src={m2} alt="" />
+          </div>
+          <h5>Harsh Gupta</h5>
+        </div>
+        <div className="members-card">
+          <div className="card-imag">
+            <img src={m3} alt="" />
+          </div>
+          <h5>Ankit Pathak</h5>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
